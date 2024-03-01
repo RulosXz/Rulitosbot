a55873a634265a56f2b2b1f95094c2dfd258a49d
BotsDeClientes <161894321+BotsDeClientes@users.noreply.github.com>
2024-03-01 17:47:26 -0500

    𝗘𝘀𝘁𝗲𝗳𝗮 𝗕𝗼𝘁 🌸

--- plugins/TeamFemes.js
+++ plugins/TeamFemes.js
@@ -5,7 +5,7 @@ handler.command = /^(TeamFem)$/i;
 export default handler;
 
 global.TeamFem = `
-🐧𝗣𝗶𝗻𝗴𝘂𝗶 𝗥𝗲𝗰𝗹𝘂𝘁𝗮𝗺𝗶𝗲𝗻𝘁𝗼𝘀 🐧
+🌸 𝗘𝘀𝘁𝗲𝗳𝗮 𝗥𝗲𝗰𝗹𝘂𝘁𝗮𝗺𝗶𝗲𝗻𝘁𝗼𝘀 🌸
 
 *Time Long Fem 💋*
 https://chat.whatsapp.com/LILrwBpgQYQ5leRfcBNwyS
