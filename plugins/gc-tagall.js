1f7cc31ead1d4d97aff558b5851ab3c3d9c9bf0d
BotsDeClientes <161894321+BotsDeClientes@users.noreply.github.com>
2024-03-01 17:29:59 -0500

    𝗘𝘀𝘁𝗲𝗳𝗮 𝗕𝗼𝘁 🌸
    𝗘𝘀𝘁𝗲𝗳𝗮 𝗕𝗼𝘁 🌸

---
+++ plugins/gc-tagall.js
@@ -0,0 +1,25 @@
+const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command}) => {
+  if (!(isAdmin || isOwner)) {
+    global.dfail('admin', m, conn);
+    throw false;
+    var sum = member.length;
+  } else {
+    var sum = 0;
+    const total = 0;
+    var member = 0;
+  }
+  const pesan = args.join` `;
+  const oi = `${pesan}`;
+  let teks = `╭┄〔 🐧 𝗣𝗶𝗻𝗴𝘂𝗶𝗕𝗼𝘁 - 𝗠𝗗 🐧 〕┄⊱\n┆\n┆ 𝗨𝘀𝘂𝗮𝗿𝗶𝗼𝘀 𝗗𝗲𝗹 𝗚𝗿𝘂𝗽𝗼 : ${participants.length} ${oi}\n┆\n┆ 𝗧𝗲 𝗔𝗰𝘁𝗶𝘃𝗮𝘀 𝗢 𝗧𝗲 𝗖𝘂𝗽𝗲𝗼 𝗣𝗹𝗮𝗻𝘁𝗶𝘁𝗮 ღ\n┆\n`;
+  for (const mem of participants) {
+    teks += `┆ღ @${mem.id.split('@')[0]}\n`;
+  }
+  teks += `└🐧 𝗣𝗶𝗻𝗴𝘂𝗶𝗕𝗼𝘁-𝗠𝗗 🐧\n\n`;
+  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
+};
+handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
+handler.tags = ['group'];
+handler.command = /^(tagall|invocar|marcar|todos|invocación|ta)$/i;
+handler.admin = true;
+handler.group = true;
+export default handler;
---
+++ plugins/info-infobot.js
@@ -0,0 +1,13 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.infopingui);
+};
+handler.command = /^(infopingui|infopingui|infopingui)$/i;
+export default handler;
+
+global.infopingui = `
+🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗜𝗻𝗳𝗼🐧
+
+𝗖𝗿𝗲𝗮𝗱𝗼𝗿 : 𝗖𝗿𝗶𝘀𝘁𝗵𝗼𝗳𝗲𝗿 𝗬𝗮𝗹𝗹𝗶𝗰𝗼
+𝗡𝘂𝗺𝗲𝗿𝗼 : +𝟱𝟭 𝟵𝟯𝟲 𝟵𝟵𝟰 𝟭𝟱𝟱
+𝗛𝗼𝘀𝘁 : 𝗖𝗮𝗳𝗶𝗿𝗲𝘅𝗼𝘀
+`;
