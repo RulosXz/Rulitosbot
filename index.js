1e87b4212498047972c4b579b7c5601ae9361ac4
BotsDeClientes <161894321+BotsDeClientes@users.noreply.github.com>
2024-03-01 17:35:01 -0500

    𝗘𝘀𝘁𝗲𝗳𝗮 𝗕𝗼𝘁 🌸

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
  
-say('Pingui\nBot', {
+say('Estefa\nBot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`BOT DE TIME LONG`, {
+say(`BOT DE PINGUI BOT`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
