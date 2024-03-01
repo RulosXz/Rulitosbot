9be948f4d28bee9ab6a3e6b42ff5e60cf63b94fb
BotsDeClientes <161894321+BotsDeClientes@users.noreply.github.com>
2024-03-01 17:47:55 -0500

    𝗘𝘀𝘁𝗲𝗳𝗮 𝗕𝗼𝘁 🌸

--- plugins/TeamMascus.js
+++ plugins/TeamMascus.js
@@ -5,7 +5,7 @@ handler.command = /^(TeamMasc)$/i;
 export default handler;
 
 global.TeamMasc = `
-🐧𝗣𝗶𝗻𝗴𝘂𝗶 𝗥𝗲𝗰𝗹𝘂𝘁𝗮𝗺𝗶𝗲𝗻𝘁𝗼𝘀 🐧
+🌸 𝗘𝘀𝘁𝗲𝗳𝗮 𝗥𝗲𝗰𝗹𝘂𝘁𝗮𝗺𝗶𝗲𝗻𝘁𝗼𝘀 🌸
 
 *🦍 Team Time Long*
 https://chat.whatsapp.com/LILrwBpgQYQ5leRfcBNwyS
