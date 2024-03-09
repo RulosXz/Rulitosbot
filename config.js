d79bfd160984703b805bc649e6d5f36ad4487aa2
BotsDeClientes <161894321+BotsDeClientes@users.noreply.github.com>
2024-03-09 02:21:31 +0100

    🌵 𝗙𝗮𝗻𝘁𝗮𝘀𝗺𝗶 𝗕𝗼𝘁 🌵

--- config.js
+++ config.js
@@ -13,9 +13,9 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-["51981983121", '𝗣𝗶𝗻𝗴𝘂𝗶 🐧', true], //𝗫𝗶𝗻𝗶𝘁𝗮 𝗕𝗼𝘁 🌸
-["5491130353033", '𝗫𝗶𝗻𝗶𝘁𝗮 🌸', true],
-["5491164708692"]]
+["51981983121", '𝗣𝗶𝗻𝗴𝘂𝗶 🐧', true], //𝗙𝗮𝗻𝘁𝗮𝘀𝗺𝗶 𝗕𝗼𝘁 🌵
+["51973965511", '🌵 𝗙𝗮𝗻𝘁𝗮𝘀𝗺𝗶 🌵', true],
+["51947311549"]]
 
 global.mods = []
 global.prems = []
@@ -112,7 +112,7 @@ global.moment = moment
 // [ES] > Agregate a ti, colaboradores o ayudates, aparecerá en el comando de lista de contactos.
 // [EN] > Adding yourself, collaborators or helpers will appear in the contact list command.
 global.official = [ // Agregate si eres Owner
-["5491130353033", '𝗫𝗶𝗻𝗶𝘁𝗮 🌸', 1]]
+["51973965511", '𝗙𝗮𝗻𝘁𝗮𝘀𝗺𝗶 𝗕𝗼𝘁 🌵', 1]]
 
 global.mail = '' // Add email
 global.desc = '' // Add short description (20 caractres max)
@@ -121,7 +121,7 @@ global.country = '' // Add country, example: 🇪🇨
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
 global.packname = "𝗧𝗼𝗺𝗮 𝗧𝘂 𝗦𝘁𝗶𝗰𝗸𝗲𝗿"
-global.author = "𝗫𝗶𝗻𝗶𝘁𝗮 𝗦𝘁𝗶𝗰𝗸𝗲𝗿 🌸"
+global.author = "𝗙𝗮𝗻𝘁𝗮𝘀𝗺𝗶 𝗦𝘁𝗶𝗰𝗸𝗲𝗿 🌵"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > CUENTAS E INFORMACIÓN DE VERSIONES DEL BOT, POR FAVOR 
@@ -129,35 +129,35 @@ global.author = "𝗫𝗶𝗻𝗶𝘁𝗮 𝗦𝘁𝗶𝗰𝗸𝗲𝗿 🌸"
 // [EN] > ACCOUNTS AND BOT VERSION INFORMATION, PLEASE KEEP THIS UNCHANGED, WE STRIVE DAILY TO PROVIDE YOU WITH A BOT FOR THE COMMUNITY, BE GRATEFUL
 global.vs = "1.7.0"
 global.vsJB = "2.5"
-global.gt = "𝗫𝗶𝗻𝗶𝘁𝗮 𝗕𝗼𝘁 🌸"
+global.gt = "🌵 𝗙𝗮𝗻𝘁𝗮𝘀𝗺𝗶 𝗕𝗼𝘁 🌵"
 
-global.yt = "https://www.instagram.com/ff.antitoo"
-global.yt2 = "https://www.instagram.com/ff.antitoo"
-global.ig = "https://www.instagram.com/ff.antitoo"
-global.md = "https://www.instagram.com/ff.antitoo"
-global.fb = "https://www.instagram.com/ff.antitoo"
-global.tk = "https://www.instagram.com/ff.antitoo"
-global.ths = "https://www.instagram.com/ff.antitoo"
+global.yt = "https://www.instagram.com/antuu.xik"
+global.yt2 = "https://www.instagram.com/antuu.xik"
+global.ig = "https://www.instagram.com/antuu.xik"
+global.md = "https://www.instagram.com/antuu.xik"
+global.fb = "https://www.instagram.com/antuu.xik"
+global.tk = "https://www.instagram.com/antuu.xik"
+global.ths = "https://www.instagram.com/antuu.xik"
 global.paypal = 'https://wa.link/tpa35w'
 global.asistencia = 'https://wa.link/tpa35w' //Contacto
 global.bot = 'https://wa.link/tpa35w' //𝗘𝘀𝘁𝗲𝗳𝗮 𝗕𝗼𝘁 🌸
 
-global.nna = 'https://www.instagram.com/ff.antitoo' // Cuentas
-global.nn2 = 'https://www.instagram.com/ff.antitoo' // Grupo tg GataBot
-global.nna2 = 'https://www.instagram.com/ff.antitoo' // Help
-global.nn = 'https://www.instagram.com/ff.antitoo' // Grupo 1
-global.nnn = 'https://www.instagram.com/ff.antitoo' // Grupo 2
-global.nnnt = 'https://www.instagram.com/ff.antitoo' // Grupo 3
-global.nnntt = 'https://www.instagram.com/ff.antitoo' // Grupo 4
-global.nnnttt = 'https://www.instagram.com/ff.antitoo' // Grupo 5
-global.nnnttt1 = 'https://www.instagram.com/ff.antitoo' // Grupo 6 COL
-global.nnnttt2 = 'https://www.instagram.com/ff.antitoo' // Grupo 7 COL
-global.nnnttt3 = 'https://www.instagram.com/ff.antitoo' // Grupo 8 COL
-global.nnnttt4 = 'https://www.instagram.com/ff.antitoo' // Grupo 9 COL
-global.nnnttt5 = 'https://www.instagram.com/ff.antitoo' // MULTI - BOTS
-global.nnnttt6 = 'https://www.instagram.com/ff.antitoo' // A.T.M.M
-global.channel1 = 'https://www.instagram.com/ff.antitoo'
-global.channel2 = 'https://www.instagram.com/ff.antitoo'
+global.nna = 'https://www.instagram.com/antuu.xik' // Cuentas
+global.nn2 = 'https://www.instagram.com/antuu.xik' // Grupo tg GataBot
+global.nna2 = 'https://www.instagram.com/antuu.xik' // Help
+global.nn = 'https://www.instagram.com/antuu.xik' // Grupo 1
+global.nnn = 'https://www.instagram.com/antuu.xik' // Grupo 2
+global.nnnt = 'https://www.instagram.com/antuu.xik' // Grupo 3
+global.nnntt = 'https://www.instagram.com/antuu.xik' // Grupo 4
+global.nnnttt = 'https://www.instagram.com/antuu.xik' // Grupo 5
+global.nnnttt1 = 'https://www.instagram.com/antuu.xik' // Grupo 6 COL
+global.nnnttt2 = 'https://www.instagram.com/antuu.xik' // Grupo 7 COL
+global.nnnttt3 = 'https://www.instagram.com/antuu.xik' // Grupo 8 COL
+global.nnnttt4 = 'https://www.instagram.com/antuu.xik' // Grupo 9 COL
+global.nnnttt5 = 'https://www.instagram.com/antuu.xik' // MULTI - BOTS
+global.nnnttt6 = 'https://www.instagram.com/antuu.xik' // A.T.M.M
+global.channel1 = 'https://www.instagram.com/antuu.xik'
+global.channel2 = 'https://www.instagram.com/antuu.xik'
 global.channel3 = 'https://chat.whatsapp.com/IKeeFD8PDaS7rkyG7a12g1'
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
@@ -183,8 +183,8 @@ global.eg = ''
 global.exito = eg
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
-global.wm = "𝗫𝗶𝗻𝗶𝘁𝗮 𝗕𝗼𝘁 🌸"
-global.igfg = "𝗫𝗶𝗻𝗶𝘁𝗮 𝗕𝗼𝘁 🌸"
+global.wm = "🌵 𝗙𝗮𝗻𝘁𝗮𝘀𝗺𝗶 𝗕𝗼𝘁 🌵"
+global.igfg = "🌵 𝗙𝗮𝗻𝘁𝗮𝘀𝗺𝗶 𝗕𝗼𝘁 🌵"
 global.wait = "*⌛ _Cargando | Charging..._ ▬▭▭▭▭▭▭*"
 global.waitt = "*⌛ _Cargando | Charging..._ ▬▬▭▭▭*"
 global.waittt = "*⌛ _Cargando | Charging..._ ▬▬▬▬▭▭*"
