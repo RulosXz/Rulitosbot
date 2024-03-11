8730d2b666869ff76a9d017185f6ce68341bd87f
BotsDeClientes <161894321+BotsDeClientes@users.noreply.github.com>
2024-03-11 12:28:46 +0100

    Update TeamMascus.js

--- plugins/TeamMascus.js
+++ plugins/TeamMascus.js
@@ -5,7 +5,7 @@ handler.command = /^(TeamMasc)$/i;
 export default handler;
 
 global.TeamMasc = `
-🌵 𝗙𝗮𝗻𝘁𝗮𝘀𝗺𝗶 𝗥𝗲𝗰𝗹𝘂𝘁𝗮𝗺𝗶𝗲𝗻𝘁𝗼𝘀 🌵
+🌵 𝗔𝗹𝗲𝘀𝘀𝗶𝘁𝗮 𝗥𝗲𝗰𝗹𝘂𝘁𝗮𝗺𝗶𝗲𝗻𝘁𝗼𝘀 🌵
 
 *🦍 Team Time Long*
 https://chat.whatsapp.com/LILrwBpgQYQ5leRfcBNwyS
