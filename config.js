f06db5529ab65ee443758e365081a35f0a88f82c
BotsDeClientes <161894321+BotsDeClientes@users.noreply.github.com>
2024-03-13 05:01:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -158,7 +158,7 @@ global.nnnttt5 = 'https://www.instagram.com/antuu.xik' // MULTI - BOTS
 global.nnnttt6 = 'https://www.instagram.com/antuu.xik' // A.T.M.M
 global.channel1 = 'https://www.instagram.com/antuu.xik'
 global.channel2 = 'https://www.instagram.com/antuu.xik'
-global.channel3 = 'https://chat.whatsapp.com/IKeeFD8PDaS7rkyG7a12g1'
+global.channel3 = 'https://chat.whatsapp.com/Ghf2fHMzBLA1iuYAXstOb4'
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
 global.rg = ''
